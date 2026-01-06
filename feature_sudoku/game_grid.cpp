9ea494190d3dec3e8753be7bbed904461dc86bd7
Kayhan Aykın <kayhan.aykin@gmail.com>
2026-01-06 15:32:03 +0300

    feat(root): Add sudoku game generator gives sudoku puzzles for different hardness options 1 to 5

---
+++ feature_sudoku/game_grid.cpp
@@ -0,0 +1,145 @@
+#include "includes.hpp"
+
+void game_generator(std::array<std::array<int, 9>, 9>& grid, int hardness_requested, SolverStats& stats)
+{
+	std::array<int, 81> mixed_sequence;
+	int temp;
+	int iterator = 0;
+	int counter = 100;
+
+
+	while (stats.hardness != hardness_requested)
+	{
+		counter++;
+		if (counter > 100)
+		{
+			fullgrid_generator(grid);
+			prepare_mixed_sequence(mixed_sequence);
+			counter = 0;
+		}
+		iterator = iterator % 81;
+
+		// std::cout << stats.hardness << ", iterator: " << iterator << std::endl;
+
+		initilize_stats(stats);
+		// delete next one
+		temp = grid[mixed_sequence[iterator] / 9][mixed_sequence[iterator] % 9]; 
+		grid[mixed_sequence[iterator] / 9][mixed_sequence[iterator] % 9] = 0;
+
+		// std::cout << counter << "  iterator: " << iterator << " cell: " << mixed_sequence[iterator] / 9 << ", " << mixed_sequence[iterator] % 9 << " empty count: " << count_empty(grid) << " solve_with_logic: " << solve_with_logic(grid, stats) << " hidden_total: " << stats.hidden_total << std::endl;
+
+		stats.empty_cell = count_empty(grid);
+		// check solvable by easy tech, if not reverse delete and skip to next cell in the sequence
+		if (stats.empty_cell < 58 && solve_with_logic(grid, stats) == false && hardness_requested != 5) 
+		{
+			// std::cout << "enter: upto 58 it has to be human solvable!" << std::endl;
+			grid[mixed_sequence[iterator] / 9][mixed_sequence[iterator] % 9] = temp;
+			iterator++;
+			continue ;
+		}
+		// hardness 1'de hidden single olmamasi gerekiyor.
+		if (hardness_requested == 1 && stats.hidden_total != 0)
+		{
+			// std::cout << "enter: hardness 1'de hidden single olmamasi lazim!" << std::endl;
+			grid[mixed_sequence[iterator] / 9][mixed_sequence[iterator] % 9] = temp;
+			iterator++;
+			continue ;
+		}
+		// check whether solution is unique if not reverse delete and skip to next cell in the sequence 
+		check_uniqueness(grid, stats);
+		if (stats.solution_count != 1)
+		{
+			// std::cout << "enter: 1 den fazla solution var!" << std::endl;
+			grid[mixed_sequence[iterator] / 9][mixed_sequence[iterator] % 9] = temp;
+			iterator++;
+			continue ;
+		}
+		determine_hardness(stats);
+		iterator++;
+	}
+}
+
+void check_uniqueness(std::array<std::array<int, 9>, 9> grid, SolverStats& stats) 
+{
+    if (stats.depth > stats.max_depth) 
+        stats.max_depth = stats.depth;
+
+    if (stats.solution_count > 1) 
+        return;
+
+    int row, col;
+    if (!find_best_empty_location(grid, row, col)) 
+    {
+        stats.solution_count++;
+        return;
+    }
+
+    int choices = 0;
+    for (int num = 1; num <= 9; num++) 
+    {
+        if (basic_check(grid, row, col, num)) 
+        {
+            stats.total_guesses++;
+            choices++;
+    
+            grid[row][col] = num;
+            stats.depth++;
+            
+            check_uniqueness(grid, stats);
+            
+            stats.depth--;
+            grid[row][col] = 0; // Backtrack
+        }
+    }
+
+    // --- DEAD END TRACKING ---
+    if (choices == 0) 
+    {
+        stats.dead_ends++;
+    }
+
+    // --- BRANCHING POINT TRACKING ---
+    if (choices > 1) 
+        stats.branching_points++;
+}
+
+void	delete_initial_cells(std::array<int, 81>& mixed_sequence, std::array<std::array<int, 9>, 9>& grid, int hardness_requested, int& i)
+{
+	int num;
+
+	switch (hardness_requested)
+	{
+	case 1:
+		num = 43;
+		break;
+	case 2:
+		num = 44;
+		break;
+	case 3:
+		num = 51;
+		break;
+	case 4:
+		num = 56;
+		break;
+	default:
+		num = 58;
+		break;
+	}
+
+	for (; i < num ; i++)
+	{
+		grid[mixed_sequence[i] / 9][mixed_sequence[i] % 9] = 0;
+	}
+}
+
+void	prepare_mixed_sequence(std::array<int, 81>& mixed_sequence)
+{
+	for (int i = 0; i < 81; i++)
+	{
+		mixed_sequence[i] = i;
+	}
+	long long seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	std::srand(static_cast<unsigned int>(seed));
+	std::random_shuffle (mixed_sequence.begin(), mixed_sequence.end());
+
+}
--- feature_sudoku/includes.hpp
+++ feature_sudoku/includes.hpp
@@ -3,20 +3,58 @@
 #include "array"
 #include "vector"
 #include <algorithm>    // std::random_shuffle
+#include <random>		// 
 #include <iostream>		// std::cout
 #include <ctime>        // std::time
+#include <iomanip>		// std::setprecision
+#include <chrono>
+
+struct SolverStats {
+	int solution_count = 0;
+	int depth = 0;
+    long long total_guesses = 0;
+    long long dead_ends = 0;
+    int max_depth = 0;
+    long long branching_points = 0;
+	int hardness = 0;
+	std::chrono::duration<double, std::milli> duration;
+	int naked_singles = 0;
+	int hidden_box = 0;
+	int hidden_row = 0;
+	int hidden_col = 0;
+	int hidden_total = 0;
+	bool solved_with_logic = false;
+	int empty_cell = 0;
+};
 
 //random_full_grid_gen.cpp
-void	sudoku_generator(std::array<std::array<int, 9>, 9>&);
+void	fullgrid_generator(std::array<std::array<int, 9>, 9>&);
 void	fill_diagonal_boxes(std::array<std::array<int, 9>, 9>&);
-void	print_sudoku(std::array<std::array<int, 9>, 9>&);
 bool 	fill_other_boxes(std::array<std::array<int, 9>, 9>&, int, int);
-bool	basic_check(const std::array<std::array<int, 9>, 9>& grid, int row, int col, int num);
-bool	find_empty_location(const std::array<std::array<int, 9>, 9>& grid, int& row, int& col);
-void	game_generator(std::array<std::array<int, 9>, 9>&);
 
 //game_grid_gen.cpp
+void	game_generator(std::array<std::array<int, 9>, 9>& game_grid, int hardness, SolverStats& stats);
+void 	check_uniqueness(std::array<std::array<int, 9>, 9> grid, SolverStats& stats);
+void	delete_initial_cells(std::array<int, 81>& mixed_sequence, std::array<std::array<int, 9>, 9>& grid, 
+	int hardness_requested, int& i);
+void	prepare_mixed_sequence(std::array<int, 81>& mixed_sequence);
 
-void	count_solutions(std::array<std::array<int, 9>, 9>& grid, int& count);
 
 //utils.cpp
+void	print_stats(const SolverStats& stats);
+void	print_grid(std::array<std::array<int, 9>, 9>&);
+void	determine_hardness(SolverStats& stats);
+void	clear_grid(std::array<std::array<int, 9>, 9>& grid);
+void	initilize_stats(SolverStats& stats);
+bool	find_best_empty_location(std::array<std::array<int, 9>, 9>& grid, int& row, int& col);
+bool	basic_check(const std::array<std::array<int, 9>, 9>& grid, int row, int col, int num);
+int		count_empty(std::array<std::array<int, 9>, 9>& grid);
+
+//tester.cpp
+void	tester();
+void	analyse_grid(SolverStats& stats, std::array<std::array<int, 9>, 9> grid);
+
+//human_logic.cpp
+bool	find_naked_single(std::array<std::array<int, 9>, 9>& grid, SolverStats& stats);
+bool	find_hidden_single(std::array<std::array<int, 9>, 9>& grid, SolverStats& stats);
+bool	solve_with_logic(std::array<std::array<int, 9>, 9> grid, SolverStats& stats);
