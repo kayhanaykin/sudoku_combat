2dabaa1e130dc9a43a4b78197e35c87237fedb97
aepContainer <ali.erenpalazz1@gmail.com>
2026-01-30 17:24:56 +0300

    fix: fix compatibility issues

--- srcs/requirements/combat_service/src/app.module.ts
+++ srcs/requirements/combat_service/src/app.module.ts
@@ -1,34 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { Room } from './room.entity';
+import { AppGateway } from './app.gateway';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true, 
-            envFilePath: '.env',
-        }),
-
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.get<string>('DB_HOST_COMBAT'),
-                port: configService.get<number>('DB_PORT_COMBAT'),
-                username: configService.get<string>('DB_USERNAME_COMBAT'),
-                password: configService.get<string>('DB_PASSWORD_COMBAT'),
-                database: configService.get<string>('DB_NAME_COMBAT'),
-                entities: [Room],
-                synchronize: true,
-            }),
-        }),
-        TypeOrmModule.forFeature([Room]),
-    ],
-    controllers: [AppController],
-    providers: [],
+	imports: [],
+	controllers: [],
+	providers: [AppGateway]
 })
 
 export class AppModule {}
--- srcs/requirements/room_service/src/main.ts
+++ srcs/requirements/room_service/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap()
 {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 8002);
+	await app.listen(8002);
 }
 
 bootstrap();
