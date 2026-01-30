dabfde4959ac9746fd77903b784be11dfe9aa8b0
aepContainer <ali.erenpalazz1@gmail.com>
2026-01-30 17:01:15 +0300

    feat(combat_service): renew combat_service implementation

--- srcs/requirements/combat_service/src/app.module.ts
+++ srcs/requirements/combat_service/src/app.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { Room } from './room.entity';
+import { AppGateway } from './app.gateway';
 
 @Module({
-	imports: [
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-			host: process.env.DB_HOST_COMBAT,
-			port: Number(process.env.DB_PORT_COMBAT),
-			username: process.env.DB_USERNAME_COMBAT,
-			password: process.env.DB_PASSWORD_COMBAT,
-			database: process.env.DB_NAME_COMBAT,
-			entities: [Room],
-			synchronize: true
-		}),
-		TypeOrmModule.forFeature([Room]),
-	],
-	controllers: [AppController],
-	providers: [],
+	imports: [],
+	controllers: [],
+	providers: [AppGateway]
 })
 
 export class AppModule {}
