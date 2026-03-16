0afe83bb032a02a5effe8447e8711ca416ce3d45
kayhan <kayhan@LaptopPC.localdomain>
2026-03-16 10:44:35 +0300

    bugfix(user_service): replace runserver with daphne

--- srcs/requirements/user_service/user_project/asgi.py
+++ srcs/requirements/user_service/user_project/asgi.py
@@ -2,13 +2,18 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from user_app.middleware import JWTAuthMiddleware
-import user_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_project.settings')
 
+# Initialize Django ASGI application early
+django_asgi_app = get_asgi_application()
+
+# Import routing and middleware AFTER get_asgi_application()
+from user_app.middleware import JWTAuthMiddleware
+import user_app.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         JWTAuthMiddleware(
             URLRouter(
