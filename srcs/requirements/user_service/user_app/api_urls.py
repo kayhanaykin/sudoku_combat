24d0c67b2b46522036fdaa281470569334635148
asteroid281 <ynseozdemir@gmail.com>
2026-03-18 18:56:46 +0300

    feat: Add user-by-username API endpoint to user service

--- srcs/requirements/user_service/user_app/api_urls.py
+++ srcs/requirements/user_service/user_app/api_urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('csrf/', api_views.get_csrf_token, name='get_csrf_token'),
     path('logout/', api_views.logout_api, name='api_logout'),
     path('info/<int:user_id>/', api_views.user_info_api, name='user_info_api'),
+    path('by-username/<str:username>/', api_views.user_by_username_api, name='user_by_username_api'),
 ]
