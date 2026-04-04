d379e2c260dd18baa2558ec9b30f1ee9afa6fc7e
aepContainer <ali.erenpalazz1@gmail.com>
2026-04-04 13:47:13 +0300

    style(combat_service): format

--- srcs/requirements/combat_service/src/app.module.ts
+++ srcs/requirements/combat_service/src/app.module.ts
@@ -8,21 +8,22 @@ import { Room } from './room.entity';
 import { RoomCleanupService } from './room-cleanup.service';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'postgres', 
-            host: 'room_db', 
-            port: 5432,
-            username: process.env.POSTGRES_USER_room,
-            password: process.env.POSTGRES_PASSWORD_room,
-            database: process.env.POSTGRES_DB_room,
-            entities: [Room],
-            synchronize: true, 
-        }),
-        TypeOrmModule.forFeature([Room]),
-        ScheduleModule.forRoot()
-    ],
-    controllers: [CombatController, AppController],
-    providers: [AppGateway, RoomCleanupService]
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres', 
+			host: 'room_db', 
+			port: 5432,
+			username: process.env.POSTGRES_USER_room,
+			password: process.env.POSTGRES_PASSWORD_room,
+			database: process.env.POSTGRES_DB_room,
+			entities: [Room],
+			synchronize: true, 
+		}),
+		TypeOrmModule.forFeature([Room]),
+		ScheduleModule.forRoot()
+	],
+	controllers: [CombatController, AppController],
+	providers: [AppGateway, RoomCleanupService]
 })
+
 export class AppModule {}
