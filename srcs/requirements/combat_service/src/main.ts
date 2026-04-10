2882ecd66093d933a684abbea08b981dd9967425
aepContainer <ali.erenpalazz1@gmail.com>
2026-04-10 17:26:34 +0300

    fix(combat_service): fix auto-removal of newly created rooms from list and style code

--- srcs/requirements/combat_service/src/main.ts
+++ srcs/requirements/combat_service/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
-async function bootstrap() 
+async function bootstrap()
 {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors(
@@ -12,7 +12,7 @@ async function bootstrap()
 		credentials: true,
 	});
 	app.useWebSocketAdapter(new WsAdapter(app));
-	await app.listen(8003, '0.0.0.0'); 
+	await app.listen(8003, '0.0.0.0');
 }
 
 bootstrap();
